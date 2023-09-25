206cb0ba3db977f8cff85474c20a91d62028879b
AashishRupakheti <145958271+AashishRupakheti@users.noreply.github.com>
2023-09-25 11:41:08 +0545

    Add files via upload

---
+++ admin_user.php
@@ -0,0 +1,220 @@
+<?php
+	include('connection.php');
+	session_start();
+	if(isset($_SESSION['userid'])){
+		$id=$_SESSION['userid'];
+	}
+
+	
+?>
+<?php	
+	//control navigation bar
+	if(isset($_SESSION['userid'])){
+
+		
+	}
+	else{
+		$signupBtn= "";
+		$loginBtn= "";
+		$cartBtn="";
+		$notifiBtn="";
+	}
+	
+	if(isset($_POST[""])){
+		session_unset();
+		header('location:index.php');
+	}
+	
+	
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>user</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="Resources/bootstrap-3.4.1-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="Resources/css/mystyle.css">
+	<link rel="stylesheet" href="Resources/css/login.css">
+	<script src="Resources/jquery/jquery.min.js"></script>
+	<script src="Resources/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+  
+
+  
+  
+</head>
+<body >
+
+
+	<!-- Navigation bar -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>                        
+				</button> 
+				<a class="navbar-brand" href="admin.php">Rupakheti General - Admin</a>
+			</div>
+			<!--<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li><?php      echo $signupBtn;                  ?></li>
+					<li><?php      echo $loginBtn;                  ?></li>
+					<li><?php      echo $notifiBtn;                  ?></li>
+					
+				</ul>
+			</div>
+		</div>-->
+	</nav>
+	<div style="height:50px"></div>
+
+
+	<!-- Header -->
+	<div class="jumbotron text-center" style="background-image: url('images/slide5.jpg');background-size: cover;margin-bottom: 0px;">
+		<h1><font color="white">Rupakheti General Suppliers</font></h1>
+		<p><font color="white">Stay Home!!</font></p>
+	</div>		
+
+	<!-- menu bar -->
+	<div>
+		<ul class="nav-justified" style="background-color:green;">
+			<li></li>
+			<li class="active"><a href="admin.php" style="color:white" >Home</a></li>
+			<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" style="color:white">Orders <span class="caret"></span></a>
+				<ul class="dropdown-menu" >
+					<li><a href="admin_orders.php?type=new">New</a></li>
+					<li><a href="admin_orders.php?type=shiped">Shiped</a></li>
+					<li><a href="admin_orders.php?type=history">History</a></li>
+				</ul>
+			</li>
+			<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" style="color:white">Items <span class="caret"></span></a>
+				<ul class="dropdown-menu" >
+					<li><a href="item.php?type=view">View / Remove</a></li>
+					<li><a href="item.php?type=add">Add New</a></li>
+				</ul>
+			</li>
+			<li><a href="admin_user.php" style="color:white">Users</a></li>
+			<!--<li><a href="about.php" style="color:white">About us</a></li>-->
+		</ul>
+	</div>
+	<hr>
+		
+	
+
+
+
+ 
+ 
+<!-- Content -->
+
+	<div class="container">
+		<div class="row container-fluid ">
+		<h2 class='text-center'>Users</h2><br>
+			<form method='post'>
+				User Id<br>
+				<input type="text" name='searchid'>
+				<input type="submit" name='search' class="loginBtn">
+			</form>
+		
+		
+			<?php
+
+				//view users
+				if(isset($_POST['search'])){
+					$usid=$_POST['searchid'];
+					$qu="SELECT * from user where user_id=$usid";
+				}else{
+					$qu="SELECT * from user";
+				}
+				
+				echo "<div class='col-md'>";
+				echo "<div style='border-radius: 25px'>";
+				echo "<table style='table-layout:fixed;width:100%'>";
+				echo "<tr>";
+				echo "<th>ID</th><th>Name</th><th>Address</th><th>Telephone</th><th>Email</th>";
+				echo "</tr>";
+				
+				$query4=$qu;
+				$result4=mysqli_query($sql_connection,$query4);
+				while($row=mysqli_fetch_assoc($result4)){
+					
+					echo "<tr>";
+					echo "<td>";
+					echo $row['user_id'];
+					echo "</td>";
+					echo "<td>";
+					echo $row['user_first_name']." ".$row['user_last_name'];
+					echo "</td>";
+					
+					echo "<td>";
+					echo $row['user_address_no']." ".$row['user_address_street']." ".$row['user_address_town'];
+					echo "</td>";
+					echo "<td>";
+					echo $row['user_tp'];
+					echo "</td>";
+					echo "<td>";
+					echo $row['user_email'];
+					echo "</td>";
+
+					$userid=$row['user_id'];
+
+					echo "<td><a href='admin_user.php?remove=$userid' class='viewbtn'>Remove</a></td>";
+					echo "</tr>";
+					echo "<tr><td colspan='6'><hr></td></tr>";
+					
+				}
+				
+				echo "</table>";
+				echo "</div>";
+				echo "</div>";
+
+				//delete user
+				 if(isset($_GET['remove'])){
+					$userid=$_GET['remove'];
+					$query5="delete from user where user_id='$userid'";
+					$result5=mysqli_query($sql_connection,$query5);
+					
+					if($result5){
+						echo '<script type="text/javascript">alert("User Removed!");window.location.href = "admin_user.php";</script>';
+					}
+				}
+
+			?>	
+			
+		</div>	
+	</div>
+
+
+	<!-- Footer -->
+	<footer class="page-footer font-small pt-4" style="background-color:green;color:white;">
+		<div style="height:20px;"></div>
+		<div class="container-fluid text-center ">
+			<div class="row">
+					<h3 class="text-uppercase">Rupakheti General Suppliers</h3>
+					<p>Natural items the effect of freshness!!!</p>
+			</div>
+		</div>
+		<div class="footer-copyright text-center py-3" style="background-color:white;color:red">
+			<h3>© 2065 Rupakheti General Suppliers. All Rights Reserved | Design by Aashish!</h3> 
+		</div>
+
+	</footer>
+	<!-- Footer -->
+
+
+
+</body>
+</html>
+
+<?php
+
+	
+	
+	mysqli_close($sql_connection);
+	
+?>
+
